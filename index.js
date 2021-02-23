11600360f880a6c6a473a6d31be9e1b424b3cc68
sporeball <sporeballdev@gmail.com>
2021-02-23 14:31:13 -0800

    setup code

---
+++ index.js
@@ -0,0 +1,14 @@
+/*
+  index.js
+  yeow core
+  copyright (c) 2021 sporeball
+  MIT license
+*/
+
+yeow = obj => {
+  let args = process.argv.slice(2);
+
+  return args;
+}
+
+module.exports = yeow;
