16e05b3ea267d4c75fcb1a10cfba70f58c33d617
sporeball <sporeballdev@gmail.com>
2021-02-23 16:11:28 -0800

    positional arguments

--- index.js
+++ index.js
@@ -12,30 +12,41 @@ yeow = obj => {
 
   for (key of Object.keys(obj)) {
     try {
-      let aliases = [`--${key}`];
-      if (obj[key].aliases) aliases = aliases.concat(obj[key].aliases.split(" / "));
-
-      if (obj[key].required && !args.some(x => aliases.includes(x))) {
-        throw new Error(`missing required argument ${key}`);
-      }
-
-      idxs[key] = args.findIndex(x => aliases.includes(x));
-
-      if (idxs[key] != -1 && obj[key].type) {
-        if (type(args[idxs[key] + 1]) != obj[key].type) {
-          throw new Error(`argument ${args[idxs[key]]} has invalid type (expected ${obj[key].type}, got ${type(args[idxs[key] + 1])})`);
+      let K = obj[key];
+      // argument is positional
+      if (K.position !== undefined) {
+        if (!args[K.position]) {
+          throw new Error(`missing required argument ${key}`);
         }
-      }
+        if (K.type && type(args[K.position]) != K.type) {
+          throw new Error(`argument ${key} has invalid type (expected ${K.type}, got ${type(args[K.position])})`)
+        }
+        a[key] = +args[K.position] || args[K.position];
+      // otherwise
+      } else {
+        let aliases = [`--${key}`];
+        if (K.aliases) aliases = aliases.concat(K.aliases.split(" / "));
 
-      switch (obj[key].type) {
-        case undefined:
-          a[key] = (idxs[key] != -1);
-          break;
-        case "string":
-          a[key] = (idxs[key] != -1) ? args[idxs[key] + 1] : obj[key].default;
-          break;
-        case "number":
-          a[key] = (idxs[key] != -1) ? +args[idxs[key] + 1] : +obj[key].default;
+        if (K.required && !args.some(x => aliases.includes(x))) {
+          throw new Error(`missing required argument ${key}`);
+        }
+
+        idxs[key] = args.findIndex(x => aliases.includes(x));
+
+        if (K.type && idxs[key] != -1 && type(args[idxs[key] + 1]) != K.type) {
+          throw new Error(`argument ${args[idxs[key]]} has invalid type (expected ${K.type}, got ${type(args[idxs[key] + 1])})`);
+        }
+
+        switch (K.type) {
+          case undefined:
+            a[key] = (idxs[key] != -1);
+            break;
+          case "string":
+            a[key] = (idxs[key] != -1) ? args[idxs[key] + 1] : K.default;
+            break;
+          case "number":
+            a[key] = (idxs[key] != -1) ? +args[idxs[key] + 1] : +K.default;
+        }
       }
     } catch (e) {
       console.log(`${red("error:")} ${e.message}`);
